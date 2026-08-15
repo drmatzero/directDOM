e2706d00bdee6c36465190441774a5ca47f61a9e
DmatLAB <drmatzeroone@gmail.com>
2026-08-16 01:45:49 +0700

    Remove exports for SVGLogo and Modal components
    Removed exports for SVGLogo and Modal components.

--- src/index.js
+++ src/index.js
@@ -11,11 +11,6 @@ export * from './html.js'; // <-- PASTIKAN NAMA FILE DI DISK ADALAH htmlElements
 // Re-export semua fungsi dan komponen router
 export * from './router.js';
 
-// Re-export komponen SVGLogo
-export * from './svg.js'; // <-- PASTIKAN NAMA FILE DI DISK ADALAH SVGLogo.js
-
-// Re-export komponen Modal
-export * from './modal.js'; // <-- PASTIKAN NAMA FILE DI DISK ADALAH Modal.js
 
 // Jika ada modul lain yang ingin Anda sertakan dalam bundel DirectDOM,
 // tambahkan di sini dengan 'export * from ...'
